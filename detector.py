0823b40a736c53b28fc457517757c24b8eb32155
gargrahul1996 <37865839+gargrahul1996@users.noreply.github.com>
2018-08-14 00:51:17 +0530

    Add files via upload

---
+++ detector.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as py
+facedetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml');
+cam=cv2.VideoCapture(0);
+rec=cv2.createLBPHFaceRecognizer();
+rec.load("recognizer/trainerData.yml")
+id=0
+font=cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_COMPLEX_SMALL,5,1,0,4)
+
+while(True):
+    ret,img=cam.read();
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = facedetect.detectMultiScale(gray,1.3,5);
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        id,conf=rec.predict(gray[y:y+h,x:x+w])
+        if(id==1):
+            id="Mohit"
+        elif(id==2):
+            id="kohli"
+        elif(id==3):
+            id="sonali"
+        elif(id==4):
+            id="Rahul"
+        
+        cv2.cv.PutText(cv2.cv.fromarray(img),str(id),(x,y+h),font,255);
+    cv2.imshow("face",img);
+    if(cv2.waitKey(1)==ord('q')):
+        break;
+cam.release()   
+cv2.destroyAllWindows()   
